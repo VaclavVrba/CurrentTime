f4c22976843a787f9d4d5e6a37297b039b06a130
VaclavVrba <101871159+VaclavVrba@users.noreply.github.com>
2024-06-05 18:01:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function showTime(){
     let minute = time.getMinutes();
     let second = time.getSeconds();
 
+    hour = hour < 10 ? '0' + hour : hour;
+    minute = minute < 10 ? '0' + minute : minute;
+    second = second < 10 ? '0' + second : second;
+
     document.getElementById('output').innerHTML = `${hour}:${minute}:${second}`;
 }
 
